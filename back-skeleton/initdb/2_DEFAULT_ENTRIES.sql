db9618cda07fed4cb694035f67d237f8d817d2f4
Harijana17 <harijana.rasiah@edu.ece.fr>
2024-03-28 11:18:26 +0100

    jeu235

---
+++ back-skeleton/initdb/2_DEFAULT_ENTRIES.sql
@@ -0,0 +1,17 @@
+INSERT INTO public.jeu_2 (id, question, option_1, option_2, option_3, reponse) VALUES (1, '_rmacie', 'fa', 'pha', 'fa', 'pharmacie');
+INSERT INTO public.jeu_2 (id, question, option_1, option_2, option_3, reponse) VALUES (2, '_and', 'c', 'qu', 'k', 'quand');
+INSERT INTO public.jeu_2 (id, question, option_1, option_2, option_3, reponse) VALUES (3, 'styl_', 'o', 'au', 'eau', 'stylo');
+INSERT INTO public.jeu_2 (id, question, option_1, option_2, option_3, reponse) VALUES (4, '_ateau', 'c', 'g', 'k', 'gateau');
+INSERT INTO public.jeu_2 (id, question, option_1, option_2, option_3, reponse) VALUES (5, 'bou_in', 'qu', 'k', 'ca', 'bouquin');
+
+INSERT INTO public.jeu_3 (id, question, reponse) VALUES (1, 'moi', 'moi');
+INSERT INTO public.jeu_3 (id, question, reponse) VALUES (2, 'tres', 'très');
+INSERT INTO public.jeu_3 (id, question, reponse) VALUES (3, 'quand', 'quand');
+INSERT INTO public.jeu_3 (id, question, reponse) VALUES (4, 'bouquin', 'bouquin');
+INSERT INTO public.jeu_3 (id, question, reponse) VALUES (5, 'typique', 'typique');
+
+INSERT INTO public.jeu_5 (id, question, option_1, option_2, option_3, reponse) VALUES (1, 'chat_', 'eau', 'au', 'o', 'chateau');
+INSERT INTO public.jeu_5 (id, question, option_1, option_2, option_3, reponse) VALUES (2, 'styl', 'o', 'eau', 'au', 'stylo');
+INSERT INTO public.jeu_5 (id, question, option_1, option_2, option_3, reponse) VALUES (3, 'boi_on', 'ch', 's', 'ss', 'boisson');
+INSERT INTO public.jeu_5 (id, question, option_1, option_2, option_3, reponse) VALUES (4, 'ch_', 'ent', 'ean', 'ant', 'chant');
+INSERT INTO public.jeu_5 (id, question, option_1, option_2, option_3, reponse) VALUES (5, 'informati', 'om', 'on', 'an', 'information');
