e1b0a6f0da7c8034be69c21ffc420c9b98bbb363
Teo-PENNECOT <97228161+Teo-PENNECOT@users.noreply.github.com>
2024-04-05 18:13:57 +0200

    Ajout de la mise en page du jeu 1 avec la bdd

--- epf-front-skeleton-main/src/app/jeu_1/jeu_1.component.ts
+++ epf-front-skeleton-main/src/app/jeu_1/jeu_1.component.ts
@@ -8,6 +8,7 @@ import { Jeu_1Service } from "services/jeu_1.service"
 @Component({
   selector: "epf-students",
   templateUrl: "./jeu_1.component.html",
+  styleUrls: ['./jeu_1.component.scss']
 })
 export class Jeu_1Component {
   jeu_1$: Observable<Jeu_1> = this._route.data.pipe(map((data) => data["jeu_1"]))
